634f2557e23c680db2963d95a562edd3db34806d
Camila Souza <camilamaiara@hotmail.com.br>
2022-12-08 15:33:20 -0300

    Update package.json

--- functions/resolve.js
+++ functions/resolve.js
@@ -7,38 +7,41 @@ mathzap.create().then(client => start(client));
 function start(client) {
     client.onMessage(async message => {
         if (message.body) {
-            const equa = message.body.toLowerCase()
-            
-            const steps = mathsteps.solveEquation(equa.replace(/\s{2,}/g, ''));
-            var resultado, passo = 1
-            const resEqua = steps.map(step => {
-                resultado = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, PASSO: "+ passo++ +" ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n\n*Equação:* " + step.oldEquation.ascii() + "\n\n*Aplicando-se:* " + step.changeType + "\n\n*Resultado:* " + step.newEquation.ascii() + "\n\n"
-                return resultado
-            })
-            await client.sendText(message.from, String(resEqua))
-
-            if (resEqua == '') {
-                const tipo = message.body.toLowerCase()
-                const steps = mathsteps.simplifyExpression(tipo.replace(/\s{2,}/g, ''));
+                try {
+                const equa = message.body.toLowerCase()
+                
+                const steps = mathsteps.solveEquation(equa.replace(/\s{2,}/g, ''));
                 var resultado, passo = 1
-                const res = steps.map(step => {
-                    resultado = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, PASSO: "+ passo++ +" ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n\n*Expressão:* " + step.oldNode.toString() + "\n\n*Aplicando-se:* " + step.changeType + "\n\n*Resultado:* " + step.newNode.toString() + "\n\n"
+                const resEqua = steps.map(step => {
+                    resultado = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, PASSO: "+ passo++ +" ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n\n*Equação:* " + step.oldEquation.ascii() + "\n\n*Aplicando-se:* " + step.changeType + "\n\n*Resultado:* " + step.newEquation.ascii() + "\n\n"
                     return resultado
                 })
-                    client.sendText(message.from, String(res))
-                    
-                    if(String(res) == ''){
-                        const res2 = math.parse(tipo)
-                    console.log(res2)
-    
-                    let scope = {
-                        x:1 
-                    } 
-                    client.sendText(message.from, '*Resultado:* ' + String(res2.eval(scope))) 
-                    }  
+                await client.sendText(message.from, String(resEqua))
+
+                if (resEqua == '') {
+                    const tipo = message.body.toLowerCase()
+                    const steps = mathsteps.simplifyExpression(tipo.replace(/\s{2,}/g, ''));
+                    var resultado, passo = 1
+                    const res = steps.map(step => {
+                        resultado = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, PASSO: "+ passo++ +" ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n\n*Expressão:* " + step.oldNode.toString() + "\n\n*Aplicando-se:* " + step.changeType + "\n\n*Resultado:* " + step.newNode.toString() + "\n\n"
+                        return resultado
+                    })
+                        client.sendText(message.from, String(res))
+                        
+                        if(String(res) == ''){
+                            const res2 = math.parse(tipo)
+                        console.log(res2)
+        
+                        let scope = {
+                            x:1 
+                        } 
+                        client.sendText(message.from, '*Resultado:* ' + String(res2.eval(scope))) 
+                        }  
+                }
+
+            }catch(err){
+                client.sendText(message.from, 'Não foi possível reconhecer a equação.')
             }
-
-
         }
     });
-} 
+}
